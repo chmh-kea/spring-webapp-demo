30fa8205693ef47325a331bf702eb6a9205810c3
chance <chmh.kea@gmail.com>
2018-03-19 23:23:54 +0100

    Initial Commit of Spring project
    Adds a boilerplate Spring Bootstrap WebApp with Web MVC and Thymeleaf
    Adds basic implementation of a controller with some actions using Thymeleaf as html preprocessor.
    Trying out Gets and Post request with various RequestParamters for getting from
    Get querystring (www.url.com/?param=value)
    Post and body (through an html form)

---
+++ src/main/java/kea/techy/demo/HomeController.java
@@ -0,0 +1,54 @@
+package kea.techy.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HomeController
+{
+    //@GetMapping("/")
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String Index()
+    {
+        return "index";
+    }
+
+    // GET
+    // localhost:8080/greetingtwo?kage=value
+    @GetMapping("/greetingtwo")
+    public String greetingTwo(@RequestParam(name="kage", defaultValue = "Empty") String name, Model model)
+    {
+        model.addAttribute("name", name);
+        return "greetingtwo";
+    }
+
+    // GET
+    // localhost:8080/user/1
+    @GetMapping("/user/{id}")
+    public String user(@PathVariable("id") int id, Model model)
+    {
+        model.addAttribute("name", id);
+        return "greetingtwo";
+    }
+
+
+
+    // POST
+    // localhost:8080/greetingtwo
+    @RequestMapping(path = "/greetingtwo", method = RequestMethod.POST)
+    //@PostMapping("/user")
+    public String useradd(@RequestParam("name") String name, Model model)
+    {
+        String temp = name;
+        model.addAttribute("name", name);
+        return "greetingtwo";
+    }
+
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
---
+++ src/main/resources/static/script.js
@@ -0,0 +1,35 @@
+var div = document.getElementById("anotherdiv");
+var data = div.getAttribute("data");
+//alert(data);
+
+var ofClass = document.getElementsByClassName("box");
+
+var divQ = $("#anotherdiv");
+var ofClassQ = $(".box");
+var allDivs = $("div");
+
+var dataQ = divQ.attr("data");
+alert(dataQ);
+
+
+
+
+// //alert("Bla");
+// var div1Value = document.getElementById("div1").getAttribute("data");
+// alert(div1Value);
+
+// var inputValue = document.getElementById("input1").value;
+// //alert(inputValue)
+
+// function validateForm() {
+//     var x = document.forms["theform"]["fname"].value;
+//     if (x == "") {
+//         alert("Name must be filled out");
+//     }
+//     else{
+//         alert("name: " + x);
+//     }
+// }
+
+
+
