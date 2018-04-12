f0358f867f001d20458f659dbde0cc70805eaaf3
chance <chmh.kea@gmail.com>
2018-04-12 05:55:57 +0200

    Adds simple single query initialization for db with users table

--- sql/INIT_DB.sql
+++ sql/INIT_DB.sql
@@ -1,3 +1,12 @@
+CREATE TABLE IF NOT EXISTS users (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `firstname` VARCHAR(45) NULL,
+  `lastname` VARCHAR(45) NULL,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `salt` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 INSERT INTO tech.users (firstname, lastname, username, password, salt) VALUES ('Jens', 'Jensen','jens1', 'notsecret', null);
 INSERT INTO tech.users (firstname, lastname, username, password, salt) VALUES ('Felix', 'Felixen','felix2', 'notsecret', null);
 INSERT INTO tech.users (firstname, lastname, username, password, salt) VALUES ('Marie', 'Mariesen','marie3', 'notsecret', null);
