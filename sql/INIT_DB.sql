e6e03dff7cc51efd6a7f0e2b8f61985bf9607c3a
chance <chmh.kea@gmail.com>
2018-04-12 10:49:42 +0200

    Fix not tech

--- sql/INIT_DB.sql
+++ sql/INIT_DB.sql
@@ -7,6 +7,6 @@ CREATE TABLE IF NOT EXISTS users (
   `salt` VARCHAR(45) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
-INSERT INTO tech.users (firstname, lastname, username, password, salt) VALUES ('Jens', 'Jensen','jens1', 'notsecret', null);
-INSERT INTO tech.users (firstname, lastname, username, password, salt) VALUES ('Felix', 'Felixen','felix2', 'notsecret', null);
-INSERT INTO tech.users (firstname, lastname, username, password, salt) VALUES ('Marie', 'Mariesen','marie3', 'notsecret', null);
+INSERT INTO users (firstname, lastname, username, password, salt) VALUES ('Jens', 'Jensen','jens1', 'notsecret', null);
+INSERT INTO users (firstname, lastname, username, password, salt) VALUES ('Felix', 'Felixen','felix2', 'notsecret', null);
+INSERT INTO users (firstname, lastname, username, password, salt) VALUES ('Marie', 'Mariesen','marie3', 'notsecret', null);
